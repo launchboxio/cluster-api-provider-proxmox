54e7e8b6fddb7d4bdcb2a4a97e8515413d4f4288
Robert Wittman <robkwittman@gmail.com>
2023-09-19 22:17:00 -0400

    Add link to issue

--- internal/machine/node_selector.go
+++ internal/machine/node_selector.go
@@ -27,6 +27,7 @@ func (m *Machine) selectNode(selector Selector) (*proxmox.NodeStatus, error) {
 
 // TODO: This is just a stub function to further filter nodes
 // to be filled out with a later update
+// Tracked by: https://github.com/launchboxio/cluster-api-provider-proxmox/issues/7
 // filterNodes removes any nodes from the list that don't have
 // available CPU, memory, or disk
 func filterNodes(nodes proxmox.NodeStatuses, machine *infrastructurev1alpha1.ProxmoxMachine) proxmox.NodeStatuses {
