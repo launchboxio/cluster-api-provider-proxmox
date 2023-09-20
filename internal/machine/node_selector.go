9a97303012c3361c3847d5cf19f400b58c2cf086
Robert Wittman <robkwittman@gmail.com>
2023-09-19 22:15:58 -0400

    Reconciler now selects random nodes if spec.targetNode not specified

---
+++ internal/machine/node_selector.go
@@ -0,0 +1,41 @@
+package machine
+
+import (
+	infrastructurev1alpha1 "github.com/launchboxio/cluster-api-provider-proxmox/api/v1alpha1"
+	"github.com/luthermonson/go-proxmox"
+	"math/rand"
+	"time"
+)
+
+type Selector interface {
+	Select(nodes proxmox.NodeStatuses) (*proxmox.NodeStatus, error)
+}
+
+// TODO: Eventually this will use a strategy for node selection
+func (m *Machine) selectNode(selector Selector) (*proxmox.NodeStatus, error) {
+	nodes, err := m.ProxmoxClient.Nodes()
+	if err != nil {
+		return nil, err
+	}
+	filteredNodes := filterNodes(nodes, m.ProxmoxMachine)
+	if selector == nil {
+		selector = &RandomNodeGroupSelector{}
+	}
+
+	return selector.Select(filteredNodes)
+}
+
+// TODO: This is just a stub function to further filter nodes
+// to be filled out with a later update
+// filterNodes removes any nodes from the list that don't have
+// available CPU, memory, or disk
+func filterNodes(nodes proxmox.NodeStatuses, machine *infrastructurev1alpha1.ProxmoxMachine) proxmox.NodeStatuses {
+	return nodes
+}
+
+type RandomNodeGroupSelector struct{}
+
+func (rng *RandomNodeGroupSelector) Select(nodes proxmox.NodeStatuses) (*proxmox.NodeStatus, error) {
+	rand.Seed(time.Now().Unix())
+	return nodes[rand.Intn(len(nodes))], nil
+}
